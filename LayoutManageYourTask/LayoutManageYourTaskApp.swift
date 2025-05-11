d4f54b95e5061ceb88c1fc447c58ef90fbc90f05
kateyatsk <klaettoe2023@gmail.com>
2025-05-11 20:40:16 +0300

    Initial Commit

---
+++ LayoutManageYourTask/LayoutManageYourTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutManageYourTaskApp.swift
+//  LayoutManageYourTask
+//
+//  Created by Екатерина Яцкевич on 11.05.25.
+//
+
+import SwiftUI
+
+@main
+struct LayoutManageYourTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
